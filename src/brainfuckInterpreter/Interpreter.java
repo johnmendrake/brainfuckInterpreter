ce06ae8dbb85b66ee7b8745f51a3a25412761eba
sarius <sarius@erebos.fritz.box>
2019-07-03 14:08:31 +0200

    added the main structure

--- src/brainfuckInterpreter/Interpreter.java
+++ src/brainfuckInterpreter/Interpreter.java
@@ -2,12 +2,12 @@ package brainfuckInterpreter;
 
 import java.io.File;
 import java.util.Scanner;
-import java.util.regex.Pattern;
 
 public class Interpreter {
 	// if too much time: replace with dynamic array list, creating indexes
 	// containing zeros when needed
 	private static int[] band = new int[30000];
+	private static int curIndex = 0;
 
 	public static void main(String[] args) {
 		try {
@@ -16,11 +16,64 @@ public class Interpreter {
 			scLine.useDelimiter("\n");
 			while (scLine.hasNext()) {
 				String curLine = scLine.next();
-				System.out.println(scLine.next());
+				System.out.println(curLine);
+				while (curLine.length() > 0) {
+					switch (curLine.substring(0, 1)) {
+					case "<":
+						decIndex();
+						break;
+					case ">":
+						incIndex();
+						break;
+					case "+":
+						incIndexValue();
+						break;
+					case "-":
+						decIndexValue();
+						break;
+					case "[":
+						break;
+					case "]":
+						break;
+					case ",":
+						break;
+					case ".":
+						printAsciiChar();
+						break;
+					default:
+						break;
+					}
+					curLine = curLine.substring(1);
+				}
 			}
 		} catch (Exception e) {
 			System.out.println("something got (brain-)fucked up");
 		}
+	}
+
+	public static void decIndex() {
+		if (curIndex > 0) {
+			curIndex--;
+		} else {
+			System.out.println("You are trying to access index < 0...");
+		}
+	}
+
+	public static void decIndexValue() {
+		band[curIndex]--;
+	}
+
+	public static void incIndex() {
+		curIndex++;
+	}
+
+	public static void incIndexValue() {
+		band[curIndex]++;
+	}
+
+	public static void printAsciiChar() {
+		// TODO: actually translate to ascii
+		System.out.print(band[curIndex]);
 	}
 
 }
